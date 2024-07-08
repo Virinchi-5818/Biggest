bced82f6057bdc1fde93ad7843e66d64cf979aa7
Virinchi-5818 <161412713+Virinchi-5818@users.noreply.github.com>
2024-07-08 13:03:55 +0530

    Add files via upload

---
+++ Pattern.java
@@ -0,0 +1,14 @@
+class Pattern
+{
+public static void main(String args[])
+{
+int n=Integer.parseInt(args[0]);
+for(i=1;i<=n;i++)
+{
+for(j=1;j<=i;j++)
+{
+System.out.print(i+"\t");
+}
+System.out.println();
+}
+}
